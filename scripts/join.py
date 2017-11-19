488f0282c5ca8cdf547744be16d4370397249574
barteksielicki <barteksielicki@gmail.com>
2017-11-19 17:12:47 +0100

    handle __title__ files in join script

--- scripts/join.py
+++ scripts/join.py
@@ -2,10 +2,15 @@
 
 import argparse
 import contextlib
+import logging
 import re
 import sys
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
+
+TITLE_FILE_NAME = '__title__.md'
+
+logger = logging.getLogger("Join script")
 
 
 @contextlib.contextmanager
@@ -25,7 +30,12 @@ def get_directory_content(dirname):
 
     nodes = [join(dirname, node) for node in listdir(dirname)
              if node_order(node)]
-    return sorted(nodes, key=node_order)
+    nodes.sort(key=node_order)
+
+    title_file = join(dirname, TITLE_FILE_NAME)
+    if exists(title_file):
+        nodes.insert(0, title_file)
+    return nodes
 
 
 def merge_directory(dirname, output, depth=0):
@@ -48,13 +58,19 @@ def adjust_headers(text, depth):
 
 def merge_file(filename, output, depth):
     with open(filename, "r") as fragment:
-        text = adjust_headers(fragment.read(), depth)
+        if TITLE_FILE_NAME in filename:
+            expected_depth = max(0, depth - 1)
+        else:
+            expected_depth = depth
+        text = adjust_headers(fragment.read(), expected_depth)
         output.write(text)
         # double new line for proper markdown rendering
         output.write("\n\n")
+        logger.info(f"Parsed file: [{filename}]; Depth: [{expected_depth}]")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument("sources_root", type=str,
                         help="path to documentation source files")
